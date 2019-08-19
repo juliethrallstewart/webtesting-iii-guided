e9f2b7d41fa25352f773ce5c92c71fc839ad4e18
Julie Thrall <juliethrall10@gmail.com>
2019-08-19 13:53:49 -0700

    testing examples have correct syntax

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Speak from './components/Speak';
+
+import { resolveSrv } from 'dns';
+import { resolve } from 'uri-js';
+
+//only use static in certain situations .. not like this one, rather
+//write test functions outs the component
+
+export const asyncFunc = () => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve('Success!');
+		}, 1000);
+	});
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	//static lets us put the function ins the app, not preferred
+	// static asyncFunc = () => {
+	// 	return new Promise(resolve => {
+	// 		setTimeout(() => {
+	// 			resolve('Success!');
+	// 		}, 1000);
+	// 	});
+	// };
+
+	state = {
+		message: ''
+	};
+
+	speak = () => {
+		this.setState({ message: 'Bark' });
+	};
+
+	render () {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and manually reload.
+					</p>
+					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+						Learn React
+					</a>
+					<Speak speak={this.speak} message={this.state.message} />
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import banana from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
 
-import App from './App';
+import App, { asyncFunc } from './App';
 
+//integration test, tests the component and the app itself
 describe('<App />', () => {
-  // 2. write this test
-  it('matches snapshot', () => {
-    const tree = banana.create(<App />); // generates a DOM tree
+	// 2. write this test
+	it('matches snapshot', () => {
+		const tree = renderer.create(<App />); // generates a DOM tree
 
-    // snapshots are a JSON representation of the DOM tree
-    expect(tree.toJSON()).toMatchSnapshot();
-  });
+		// snapshots are a JSON representation of the DOM tree
+		expect(tree.toJSON()).toMatchSnapshot();
+	});
+});
+
+//unit test, like a utility function running some JS
+describe('asyncFunc', () => {
+	it('eventually resolves to success message', () => {
+		let resolvedValue = null;
+		const expected = 'Success!';
+		asyncFunc().then(res => {
+			resolvedValue = res;
+			expect(resolvedValue).toEqual(expected);
+		});
+	});
 });
